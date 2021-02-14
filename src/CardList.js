8e086b8fb8af2a0e62ea7d940fe8e3105eb2e379
Bharat Dave <davebharat93@gmail.com>
2021-02-13 23:13:56 -0500

    Create array of Card components and display them for each robot

--- src/CardList.js
+++ src/CardList.js
@@ -1,15 +1,13 @@
 import React from 'react'
 import Card from './Card';
 
-const CardList = (props)=>{
+const CardList = ({ robots })=>{
 
-	return(
-			<div>
-				<Card id={props.robots[0].id} name={props.robots[0].name} email={props.robots[0].email}/>
-    			<Card id={props.robots[1].id} name={props.robots[1].name} email={props.robots[1].email}/>
-    			<Card id={props.robots[2].id} name={props.robots[2].name} email={props.robots[2].email}/>
-    		</div>
-		);
+	const cardsArray=robots.map((robot,i)=>{
+		return <Card id={robot.id} name={robot.name} email={robot.email}/>
+	});	
+						
+	return (<div>{cardsArray}</div>);
 }
 
 export default CardList;
