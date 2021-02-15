c54f0b536b5ade7d78c90ca69f259a789127be33
Bharat Dave <davebharat93@gmail.com>
2021-02-15 15:37:35 -0500

    Convert App component from function type to class type

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { robots } from './robots'
 import CardList from './CardList'
 import Searchbox from './Searchbox'
 
-const App = ()=>{
-	return(
-			<div className="tc">
-				<h1>Robofriends</h1>
-				<Searchbox />
-				<CardList robots={robots}/>
-			</div>
-				
-		);
+
+class App extends Component{
+
+	render(){
+		return(
+				<div className='tc'>
+					<h1>Robofriends</h1>
+					<Searchbox />
+					<CardList robots={robots}/>
+				</div>
+					
+			);
+	}
+
 }
 
 export default App;
