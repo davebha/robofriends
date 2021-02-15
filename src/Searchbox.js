f0bbc41734a9b3105fa445f6e01d86fc479394f6
Bharat Dave <davebharat93@gmail.com>
2021-02-15 15:20:49 -0500

    Change double quotes to single onesfor values

--- src/Searchbox.js
+++ src/Searchbox.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Searchbox = ()=>{
 	return(
-		<div className="pa2">
-			<input className="pa3 ba b--green bg-lightest-blue" type="search" placeholder="search robots" />
+		<div className='pa2'>
+			<input className='pa3 ba b--green bg-lightest-blue' type='search' placeholder='search robots' />
 		</div>)
 
 }
