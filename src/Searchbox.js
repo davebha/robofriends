bf360f59f952005f00541eefefb678020f783bcc
Bharat Dave <davebharat93@gmail.com>
2021-02-15 15:49:02 -0500

    Use OnChange event listener to display search values onto console

--- src/Searchbox.js
+++ src/Searchbox.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Searchbox = ({ searchChange })=>{
 	return(
 		<div className='pa2'>
-			<input className='pa3 ba b--green bg-lightest-blue' type='search' placeholder='search robots' />
+			<input onChange={searchChange} className='pa3 ba b--green bg-lightest-blue' type='search' placeholder='search robots' />
 		</div>)
 
 }
