fb3178ee7c6639c05f6a9f2f4131edf3c2318191
Bharat Dave <davebharat93@gmail.com>
2021-02-18 21:10:52 -0500

    fetch users,convert it to JSON and update robots state with users using setState

--- src/App.js
+++ src/App.js
@@ -14,7 +14,9 @@ class App extends Component{
 	}
 
 	componentDidMount(){
-
+		fetch('https://jsonplaceholder.typicode.com/users')
+		.then(response=>response.json())
+		.then(users=>this.setState({robots:users}));
 		
 	}
 	onSearchChange=(event)=>{
