99e05d902f861826a1ff52d6063aa64e72bed5ad
Bharat Dave <davebharat93@gmail.com>
2021-02-19 14:25:42 -0500

    Add styling the Scroll Div wrapper to make it scrollable

--- src/Scroll.js
+++ src/Scroll.js
@@ -4,7 +4,7 @@ import React from 'react'
 const Scroll = (props)=>{
 
 	return(
-	<div>
+	<div style={{overflowY:'scroll',height:'1000px',background:'rgba(0,128,128,1)'}}>
 		{props.children}
 	</div>);
 }
