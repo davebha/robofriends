9a1089b72e295b2912bc0b8a42a762cd14a76e93
Bharat Dave <davebharat93@gmail.com>
2021-02-18 21:21:13 -0500

    Add the loading text if the there is no data to pull from

--- src/App.js
+++ src/App.js
@@ -26,14 +26,19 @@ class App extends Component{
 		const filteredRobots = this.state.robots.filter(robot=>{
 			return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
 		});
-		return(
-				<div className='tc'>
-					<h1 className='f1'>Robofriends</h1>
-					<Searchbox searchChange={this.onSearchChange}/>
-					<CardList robots={filteredRobots}/>
-				</div>
-					
-			);
+
+		if (this.state.robots.length ===0){
+			return <h1 className='tc'>Loading...</h1>;
+		}else{
+			return(
+					<div className='tc'>
+						<h1 className='f1'>Robofriends</h1>
+						<Searchbox searchChange={this.onSearchChange}/>
+						<CardList robots={filteredRobots}/>
+					</div>
+						
+				);
+		}
 	}
 
 }
